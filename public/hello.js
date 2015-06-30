15078f26f110ecf3b85cc74b0d959d896f5d3696
Will Humphreys <will.humphreys@bbc.co.uk>
2015-06-30 15:41:03 +0100

    Rework the controller

--- public/hello.js
+++ public/hello.js
@@ -1,6 +1,18 @@
-function Hello($scope, $http) {
-    $http.get('http://localhost:8080/localfiles').
-        success(function(data) {
-            $scope.localfiles = data;
+var myApp = angular.module('myApp',[]);
+
+myApp.service('dataService', function($http) {
+    this.getData = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/localfiles'
         });
-}
+    }
+});
+
+myApp.controller('Hello', function($scope, dataService) {
+    $scope.data = null;
+    dataService.getData().then(function(dataResponse) {
+        $scope.data = dataResponse;
+    });
+});
+
