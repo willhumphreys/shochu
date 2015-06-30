7cd9221d59a6bc3870c3f453c7b7736140e44d5f
Will Humphreys <will.humphreys@bbc.co.uk>
2015-06-30 15:47:28 +0100

    List remote files

--- public/hello.js
+++ public/hello.js
@@ -1,18 +1,30 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.service('dataService', function($http) {
-    this.getData = function() {
+    this.getLocalFiles = function() {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/localfiles'
         });
-    }
+    };
+
+    this.getRemoteFiles = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/remotefiles'
+        });
+    };
 });
 
 myApp.controller('Hello', function($scope, dataService) {
-    $scope.data = null;
-    dataService.getData().then(function(dataResponse) {
-        $scope.data = dataResponse;
+    $scope.localfiles = null;
+    dataService.getLocalFiles().then(function(dataResponse) {
+        $scope.localfiles = dataResponse;
+    });
+
+    $scope.remotefiles = null;
+    dataService.getRemoteFiles().then(function(dataResponse) {
+        $scope.remotefiles = dataResponse;
     });
 });
 
