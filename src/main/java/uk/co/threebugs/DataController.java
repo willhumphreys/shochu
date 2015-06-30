3f6f248ccafd871e3969e4283a6d65825f83ea2a
Will Humphreys <will.humphreys@bbc.co.uk>
2015-06-30 15:19:59 +0100

    Add remote files endpoint. XXXXX We are logging in on each call. Probably wrong.

--- src/main/java/uk/co/threebugs/DataController.java
+++ src/main/java/uk/co/threebugs/DataController.java
@@ -1,10 +1,13 @@
 package uk.co.threebugs;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import uk.co.threebugs.service.BucketService;
 import uk.co.threebugs.service.BucketServiceImpl;
+import uk.co.threebugs.service.LocalFileService;
 import uk.co.threebugs.service.LocalFileServiceImpl;
 
 import java.util.Set;
@@ -12,11 +15,11 @@ import java.util.Set;
 @Controller
 public class DataController {
 
-    private final LocalFileServiceImpl localFileService;
-    private final BucketServiceImpl bucketService;
+    private final LocalFileService localFileService;
+    private final BucketService bucketService;
 
     @Autowired
-    public DataController(LocalFileServiceImpl localFileService, BucketServiceImpl bucketService) {
+    public DataController(LocalFileService localFileService, BucketService bucketService) {
         this.localFileService = localFileService;
         this.bucketService = bucketService;
     }
@@ -25,5 +28,12 @@ public class DataController {
     public @ResponseBody
     Set<String> getLocalFiles() {
         return localFileService.listLocalFiles();
+    }
+
+    @RequestMapping("/remotefiles")
+    public @ResponseBody
+    Set<String> getRemoteFiles() {
+        final AmazonS3 amazonS3 = bucketService.loginAWS();
+        return bucketService.listRemoteFiles(amazonS3, "tickdata-matcha");
     }
 }
