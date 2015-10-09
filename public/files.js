712260069b1525365d91fc4bf821ccbc2cefe52a
Will Humphreys <will.humphreys@bbc.co.uk>
2015-10-09 12:41:48 +0100

    Add code to retrieve live data

--- public/files.js
+++ public/files.js
@@ -1,30 +1,58 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.service('dataService', function($http) {
-    this.getLocalFiles = function() {
+    this.getLocalTickDataFiles = function() {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/localfiles/tickData'
         });
     };
 
-    this.getRemoteFiles = function() {
+    this.getRemoteTickDataFiles = function() {
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/remotefiles/tickdata-matcha'
         });
     };
+
+    this.getLocalLiveDataFiles = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/localfiles/liveData'
+        });
+    };
+
+    this.getRemoteLiveDataFiles = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/remotefiles/livedata-matcha'
+        });
+    };
 });
 
 myApp.controller('FileController', function($scope, dataService) {
-    $scope.localfiles = null;
-    dataService.getLocalFiles().then(function(dataResponse) {
-        $scope.localfiles = dataResponse;
+
+    //Local Data
+    $scope.localTickDataFiles = null;
+    dataService.getLocalTickDataFiles().then(function(dataResponse) {
+        $scope.localTickDataFiles = dataResponse;
     });
 
-    $scope.remotefiles = null;
-    dataService.getRemoteFiles().then(function(dataResponse) {
-        $scope.remotefiles = dataResponse;
+    $scope.remoteTickDataFiles = null;
+    dataService.getRemoteTickDataFiles().then(function(dataResponse) {
+        $scope.remoteTickDataFiles = dataResponse;
     });
+
+    //Live Data
+    $scope.localLiveDataFiles = null;
+    dataService.getLocalLiveDataFiles().then(function(dataResponse) {
+        $scope.localLiveDataFiles = dataResponse;
+    });
+
+    $scope.remoteLiveDataFiles = null;
+    dataService.getRemoteLiveDataFiles().then(function(dataResponse) {
+        $scope.remoteLiveDataFiles = dataResponse;
+    });
+
 });
 
